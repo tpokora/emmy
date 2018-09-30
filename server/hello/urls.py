b103c698880fb3df1e662f0d28fd43e07dd8c828
tpokora <pokora.tomek@gmail.com>
2018-09-30 12:37:09 +0200

    Class Based Views

--- server/hello/urls.py
+++ server/hello/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url('normal', views.get_hello, name='hello'),
     url('serialized', views.get_hello_serialized, name='helloSerialized'),
-    url(r'^dog/$', views.dogs),
-    url(r'^dog/(?P<pk>[0-9]+)/$', views.dog_detail),
+    url(r'^dog/$', views.DogsList.as_view()),
+    url(r'^dog/(?P<pk>[0-9]+)/$', views.DogDetails.as_view()),
     url('dog_clear', views.dogs_clear)
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- server/hello/views.py
+++ server/hello/views.py
@@ -1,11 +1,10 @@
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 
 from .models import Hello, Dog
 from .serializers import HelloSerializer, DogSerializer
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse, Http404
 from rest_framework import status
-from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 
 
@@ -14,42 +13,26 @@ def get_hello(request):
     return JsonResponse({'content': hello.content})
 
 
+@api_view(['GET'])
 def get_hello_serialized(request):
     hello = Hello(content='Welcome to Emmy Rose application! - Serialized!')
     serializer = HelloSerializer(hello)
-    return JsonResponse(serializer.data)
+    return Response(serializer.data)
 
 
-@api_view(['GET', 'POST'])
-def dogs(request):
-    if request.method == 'GET':
+class DogsList(APIView):
+
+    def get(self, request, format=None):
         dogs = Dog.objects.all()
         serializer = DogSerializer(dogs, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = DogSerializer(data=data)
+    def post(self, request, format=None):
+        serializer = DogSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view((['GET', 'DELETE']))
-def dog_detail(request, pk):
-    try:
-        dog = Dog.objects.get(pk=pk)
-    except Dog.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = DogSerializer(dog)
-        return Response(serializer.data)
-
-    if request.method == 'DELETE':
-        dog.delete()
-        return Response(status.HTTP_204_NO_CONTENT)
 
 
 @api_view(['DELETE'])
@@ -58,4 +41,23 @@ def dogs_clear(request):
         dogs = Dog.objects.all()
         dogs.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class DogDetails(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Dog.objects.get(pk=pk)
+        except Dog.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        serializer = DogSerializer(self.get_object(pk))
+        return Response(serializer.data)
+
+    def delete(self, request, pk, format=None):
+        dog = self.get_object(pk)
+        dog.delete()
+        return Response(status.HTTP_204_NO_CONTENT)
+
 
