4e48ba8976903ec3ec3b4362c7b7e3a117352f51
tpokora <pokora.tomek@gmail.com>
2018-12-16 15:31:16 +0100

    Get user by username

--- emmy_django/emmy_rose/blog/views.py
+++ emmy_django/emmy_rose/blog/views.py
@@ -1,9 +1,12 @@
 # Create your views here.
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from emmy_rose.blog.models import Entry
 from emmy_rose.blog.serializers import UserSerializer, GroupSerializer, EntrySerializer
+from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -12,6 +15,11 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+
+    @action(methods=['get'], detail=False, url_path='username/(?P<username>\w+)')
+    def get_by_username(self, request, username=None):
+        user = get_object_or_404(User, username=username)
+        return Response(UserSerializer(user, context={'request': request}).data, status=status.HTTP_200_OK)
 
 
 class GroupViewSet(viewsets.ModelViewSet):
