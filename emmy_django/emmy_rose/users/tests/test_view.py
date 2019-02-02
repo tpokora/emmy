0744b8cb065892e78f70cd6e396d2081f7109fec
tpokora <pokora.tomek@gmail.com>
2019-02-02 18:21:21 +0100

    UserViews tests

--- emmy_django/emmy_rose/users/tests/test_view.py
+++ emmy_django/emmy_rose/users/tests/test_view.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 from emmy_rose.common.tests.helpers import login
-from emmy_rose.users.views import UserViewSet, UserSerializer
+from emmy_rose.users.views import UserViewSet, UserSerializer, UserSummarySerializer
 from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.test import APIRequestFactory
 
+user_get_by_username = UserViewSet.as_view({'get': 'get_by_username'})
+user_get_details_by_username = UserViewSet.as_view({'get': 'get_details_by_username'})
 user_get_user_by_token = UserViewSet.as_view({'post': 'get_user_by_token'})
 
 
@@ -20,11 +22,31 @@ class GetUserByTokenTest(TestCase):
         self.token = login(self.FIRST_USERNAME, self.FIRST_PASSWORD)
 
     def test_get_user_by_username(self):
+        url = '/users/username/{username}'.format(username=self.user.username)
+        response = user_get_by_username(get_request(url, self.token), username=self.user.username)
+        serializer = UserSummarySerializer(self.user, many=False, context={'request': Request(get_request(url, self.token))})
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_user_details_by_username(self):
+        url = '/username/{username}/details'.format(username=self.user.username)
+        response = user_get_details_by_username(get_request(url, self.token), username=self.user.username)
+        serializer = UserSerializer(self.user, many=False, context={'request': Request(get_request(url, self.token))})
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_token_by_username(self):
         url = '/users/token'
         response = user_get_user_by_token(post_request(url, self.token))
         serializer = UserSerializer(self.user, many=False, context={'request': Request(post_request(url, self.token))})
         self.assertEqual(response.data, serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+def get_request(url, token):
+    factory = APIRequestFactory()
+    request = factory.get(url, HTTP_AUTHORIZATION='Bearer %s' % token)
+    return request
 
 
 def post_request(url, token):
