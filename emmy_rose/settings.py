772bbb35f46c8c5687228e22fab6b8d7d1355510
tpokora <pokora.tomek@gmail.com>
2019-02-03 14:36:17 +0100

    #2 - Heroku config

--- emmy_rose/settings.py
+++ emmy_rose/settings.py
@@ -132,7 +132,7 @@ class BaseConfiguration(Configuration):
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATIC_URL = '/static/'
 
     # Extra places for collectstatic to find static files.
