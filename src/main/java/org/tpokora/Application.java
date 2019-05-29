9484d750d67919bb3e6cc95ac7dbc34f1aeada35
tpokora <pokora.tomek@gmail.com>
2019-05-29 20:24:42 +0200

    #6 - Postgresql spring setup

--- src/main/java/org/tpokora/Application.java
+++ src/main/java/org/tpokora/Application.java
@@ -4,11 +4,13 @@ import java.util.Arrays;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) {
