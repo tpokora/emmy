a248e7e8950db3fec3fd104ff8997a7777380bea
tpokora <pokora.tomek@gmail.com>
2019-06-26 19:56:27 +0200

    #11 Add UserController and StormController Swagger annotations

--- src/main/java/org/tpokora/users/web/UserController.java
+++ src/main/java/org/tpokora/users/web/UserController.java
@@ -1,5 +1,9 @@
 package org.tpokora.users.web;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
@@ -12,12 +16,16 @@ import org.tpokora.users.model.User;
 import java.util.List;
 
 @Profile("!nodb")
+@Api(value = "User controller", description = "User API")
 @RestController
 public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     UserRepository userRepository;
 
+    @ApiOperation(value = "Get users", notes = "Returns all users")
     @RequestMapping("/users")
     public ResponseEntity<List<User>> getAllUsers() {
         List<User> userList = userRepository.findAll();
