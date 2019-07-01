b25f1d0c41afa3bee7eecd5072cf4868bc10a92a
tpokora <pokora.tomek@gmail.com>
2019-07-01 19:28:29 +0200

    #8 Separate Warnings

--- src/main/java/org/tpokora/storms/web/StormController.java
+++ src/main/java/org/tpokora/storms/web/StormController.java
@@ -23,6 +23,7 @@ import org.tpokora.storms.services.FindWarningService;
 import javax.xml.soap.SOAPException;
 import javax.xml.soap.SOAPFault;
 import javax.xml.soap.SOAPMessage;
+import java.util.Set;
 
 
 @Api(value = "Storms", description = "Storms API")
@@ -84,7 +85,7 @@ public class StormController {
         Coordinates coordinates = new Coordinates(x, y);
         SOAPMessage warningResponse = findWarningService.findWarning(coordinates);
 
-        Warning warning = findWarningService.handleResponse(warningResponse);
+        Set<Warning> warning = findWarningService.handleResponse(warningResponse);
         return new ResponseEntity<>(warning, HttpStatus.OK);
     }
 
