52921bd5b418f0573b9d5a08a5a22fc29cc8d085
tpokora <pokora.tomek@gmail.com>
2019-07-03 20:04:28 +0200

    Property model

---
+++ src/main/java/org/tpokora/config/Property.java
@@ -0,0 +1,29 @@
+package org.tpokora.config;
+
+public class Property {
+    private String property;
+    private String value;
+
+    public Property() {}
+
+    public Property(String property, String value) {
+        this.property = property;
+        this.value = value;
+    }
+
+    public String getProperty() {
+        return property;
+    }
+
+    public void setProperty(String property) {
+        this.property = property;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
