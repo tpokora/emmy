448235c67447eab4d14808b80e4d7397eb85a113
tpokora <pokora.tomek@gmail.com>
2019-07-15 19:16:02 +0200

    #12 Small refactor + not finished registration view

--- src/main/java/org/tpokora/views/HomeView.java
+++ src/main/java/org/tpokora/views/HomeView.java
@@ -2,6 +2,7 @@ package org.tpokora.views;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
@@ -11,14 +12,21 @@ public class HomeView extends VerticalLayout {
     public static final String ROUTE = "home";
     public static final String WELCOME_TO_EMMY_APP = "Welcome to Emmy App!";
     public static final String CONFIG_BTN_TEXT = "Config";
+    public static final String ADD_USER_BTN_TEXT = "Create User";
 
     public HomeView() {
         Button configBtn = new Button(CONFIG_BTN_TEXT);
         configBtn.addClickListener(e -> {
             configBtn.getUI().ifPresent(ui -> ui.navigate("config"));
         });
+        Button addUserBtn = new Button(ADD_USER_BTN_TEXT);
+        addUserBtn.addClickListener(e -> {
+            addUserBtn.getUI().ifPresent(ui -> ui.navigate("signup"));
+        });
+        HorizontalLayout buttonsLayout = new HorizontalLayout();
+        buttonsLayout.add(configBtn, addUserBtn);
         add(
                 new H1(WELCOME_TO_EMMY_APP),
-                configBtn);
+                buttonsLayout);
     }
 }
---
+++ src/main/java/org/tpokora/views/RegistrationView.java
@@ -0,0 +1,40 @@
+package org.tpokora.views;
+
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.NativeButton;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
+import com.vaadin.flow.router.Route;
+
+@Route(value = RegistrationView.ROUTE)
+public class RegistrationView extends VerticalLayout {
+    public static final String ROUTE = "signup";
+    //TODO: finish registration form
+    public RegistrationView() {
+        FormLayout layout = new FormLayout();
+        TextField loginField = new TextField();
+        loginField.setValueChangeMode(ValueChangeMode.EAGER);
+        TextField emailField = new TextField();
+        emailField.setValueChangeMode(ValueChangeMode.EAGER);
+        TextField passwordField = new TextField();
+        passwordField.setValueChangeMode(ValueChangeMode.EAGER);
+
+        NativeButton save = new NativeButton("Save");
+        NativeButton reset = new NativeButton("Reset");
+
+        layout.addFormItem(loginField, "Login");
+        layout.addFormItem(emailField, "Email");
+        layout.addFormItem(passwordField, "Password");
+
+        HorizontalLayout actions = new HorizontalLayout();
+        actions.add(save, reset);
+
+        layout.add(actions);
+
+        getElement().appendChild(layout.getElement());
+    }
+
+
+}
