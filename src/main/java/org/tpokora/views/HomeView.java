a17690ef0ef2a20665aad7af491fd4fe3196b78b
tpokora <pokora.tomek@gmail.com>
2019-07-20 19:28:55 +0200

    #24 User AppLayout

--- src/main/java/org/tpokora/views/HomeView.java
+++ src/main/java/org/tpokora/views/HomeView.java
@@ -1,15 +1,15 @@
 package org.tpokora.views;
 
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.AppLayoutMenu;
+import com.vaadin.flow.component.applayout.AppLayoutMenuItem;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.router.Route;
-import org.tpokora.views.main.Footer;
-import org.tpokora.views.main.Header;
-import org.tpokora.views.main.Main;
 
 @Route(value = HomeView.ROUTE)
-public class HomeView extends VerticalLayout {
+public class HomeView extends AppLayout {
 
 
     public static final String ROUTE = "home";
@@ -18,36 +18,13 @@ public class HomeView extends VerticalLayout {
     public static final String ADD_USER_BTN_TEXT = "Create User";
 
     public HomeView() {
-//        Button configBtn = new Button(CONFIG_BTN_TEXT);
-//        configBtn.addClickListener(e -> {
-//            configBtn.getUI().ifPresent(ui -> ui.navigate("config"));
-//        });
-//        Button addUserBtn = new Button(ADD_USER_BTN_TEXT);
-//        addUserBtn.addClickListener(e -> {
-//            addUserBtn.getUI().ifPresent(ui -> ui.navigate("signup"));
-//        });
-//        HorizontalLayout buttonsLayout = new HorizontalLayout();
-//        buttonsLayout.add(configBtn, addUserBtn);
-//        layout.add(
-//                new H1(WELCOME_TO_EMMY_APP),
-//                buttonsLayout);
-        getStyle().set("border", "1px solid #9E9E9E");
-        setDefaultHorizontalComponentAlignment(
-                FlexComponent.Alignment.STRETCH);
+        AppLayoutMenu menu = createMenu();
 
-        createComponents();
-    }
+        menu.addMenuItems(new AppLayoutMenuItem(ADD_USER_BTN_TEXT, "signup"),
+                new AppLayoutMenuItem(CONFIG_BTN_TEXT, "config"));
 
-    private void createComponents() {
-        Div header = new Header();
-        expand(header);
-
-        Div main = new Main();
-        setFlexGrow(2, main);
-
-        Div footer = new Footer();
-        setFlexGrow(0.5, footer);
-
-        add(header, main, footer);
+        Component content = new Span(new H3("Page title"),
+                new Span("Page content"));
+        setContent(content);
     }
 }
