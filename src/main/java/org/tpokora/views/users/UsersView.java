17bc127bca157ecf438ab0ee21fb12f84d0ac592
tpokora <pokora.tomek@gmail.com>
2019-07-21 20:54:10 +0200

    #26 Users list in grid

--- src/main/java/org/tpokora/views/users/UsersView.java
+++ src/main/java/org/tpokora/views/users/UsersView.java
@@ -3,6 +3,7 @@ package org.tpokora.views.users;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.details.Details;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Span;
@@ -10,12 +11,15 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.tpokora.config.model.Property;
 import org.tpokora.users.model.User;
 import org.tpokora.users.services.UserDetailsServiceImpl;
 import org.tpokora.views.AbstractView;
 import org.tpokora.views.MainView;
 import org.tpokora.views.common.RouteStrings;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Tag("users-view")
@@ -23,28 +27,32 @@ import java.util.List;
 @PageTitle(RouteStrings.USERS)
 public class UsersView extends AbstractView {
 
-    VerticalLayout verticalLayout = new VerticalLayout();
-    List<User> userList;
+    private UserDetailsServiceImpl userDetailsService;
+    private VerticalLayout verticalLayout = new VerticalLayout();
+    private Grid<UserRow> grid;
+
 
     public UsersView(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
         this.verticalLayout.add(new H3(RouteStrings.USERS));
-        this.userList = userDetailsService.gettAllUsers();
-
-        for (User user : this.userList) {
-            VerticalLayout userDetails = new VerticalLayout();
-            userDetails.add(
-                    new Span(String.format("Email: %s", user.getEmail())),
-                    new Span(String.format("Role: %s", user.getRoles().stream().findFirst().get().getName())));
-            Details userDetailsElement = new Details(user.getUsername(),
-                    userDetails);
-//        show notification
-//        userDetailsElement.addOpenedChangeListener(e ->
-//                Notification.show(e.isOpened() ? "Opened" : "Closed"));
-
-            this.verticalLayout.add(userDetailsElement);
-        }
+        setupUsersGrid();
+        this.verticalLayout.add(this.grid);
 
         setupContentDefaultStyles();
         addToContent(this.verticalLayout);
+    }
+
+    private void setupUsersGrid() {
+        this.grid = new Grid<>(UserRow.class);
+        this.grid.setColumns("username", "email", "role");
+        this.grid.setItems(getUserRows(this.userDetailsService.gettAllUsers()));
+    }
+
+    private ArrayList<UserRow> getUserRows(List<User> userList) {
+        ArrayList<UserRow> userRows = new ArrayList<>();
+        for (User user : userList) {
+            userRows.add(new UserRow(user.getUsername(), user.getEmail(), user.getRoles().stream().findFirst().get().getName()));
+        }
+        return userRows;
     }
 }
