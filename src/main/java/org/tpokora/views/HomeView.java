47dab1715c3f00550941c92cb00e32672a88fd13
tpokora <pokora.tomek@gmail.com>
2019-07-21 15:11:23 +0200

    #24 HomeView

--- src/main/java/org/tpokora/views/HomeView.java
+++ src/main/java/org/tpokora/views/HomeView.java
@@ -2,6 +2,7 @@ package org.tpokora.views;
 
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import org.tpokora.views.common.RouteStrings;
@@ -11,7 +12,17 @@ import org.tpokora.views.common.RouteStrings;
 @PageTitle(RouteStrings.HOME)
 public class HomeView extends Div {
 
+    public static final String WELCOME_TO_EMMY_APP = "Welcome to Emmy App!";
+    private Div homeContent;
+
     public HomeView() {
-        setText("HOME");
+        this.homeContent = new Div();
+        setupContent();
+        add(homeContent);
+    }
+
+    private void setupContent() {
+        this.homeContent.getStyle().set("padding", "30px");
+        this.homeContent.add(new H3(WELCOME_TO_EMMY_APP));
     }
 }
