e4ae0f36bc6b7ca44a04dfa550cbe1bcc309a4eb
tpokora <pokora.tomek@gmail.com>
2019-09-03 19:59:45 +0200

    #28 - Generate main Navbar

--- src/main/java/org/tpokora/views/MainView.java
+++ src/main/java/org/tpokora/views/MainView.java
@@ -2,6 +2,7 @@ package org.tpokora.views;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.page.Viewport;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
@@ -17,19 +18,25 @@ import static javax.swing.ScrollPaneConstants.VIEWPORT;
 public class MainView extends AbstractView implements RouterLayout {
 
     public static final String EMMY_APP = "EmmyApp";
+    private AppLayout appLayout;
 
     public MainView() {
+        this.appLayout = new AppLayout();
+        addToContent(appLayout);
+        createNavbar();
+    }
+
+    private void createNavbar() {
         Tabs tabs = new Tabs();
         tabs.setOrientation(Tabs.Orientation.HORIZONTAL);
-        AppLayout appLayout = new AppLayout();
-        appLayout.addToNavbar(true, tabs);
+
+        Span appName = new Span(EMMY_APP);
+        appLayout.addToNavbar(true, appName, tabs);
 
         tabs.add(createTab(HomeView.class, RouteStrings.HOME));
         tabs.add(createTab(UsersView.class, RouteStrings.USERS));
         tabs.add(createTab(ConfigView.class, RouteStrings.CONFIG));
         tabs.add(createTab(WeatherView.class, RouteStrings.WEATHER));
-
-        addToContent(appLayout);
     }
 
     private Tab createTab(Class<? extends Component> navigationTarget, String linkText) {
