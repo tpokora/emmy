02ffc4ef1ccf48074cbfdcd566965b0c466a33ab
tpokora <pokora.tomek@gmail.com>
2019-10-20 17:36:29 +0200

    #31 - HomeView basic test

---
+++ src/test/java/org/tpokora/views/BasicTest.java
@@ -0,0 +1,72 @@
+package org.tpokora.views;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.tpokora.config.properties.TestProperties;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = TestProperties.class)
+@TestPropertySource("classpath:application-test.properties")
+public class BasicTest {
+
+    public static final String WELCOME_TO_EMMY_APP = "Welcome to Emmy App!";
+
+    @Autowired
+    private TestProperties testProperties;
+
+    private WebDriver driver;
+
+    @Before
+    public void setup(){
+        String pathToChromeDriver = "lib/chromedriver/chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", pathToChromeDriver);
+        this.driver = new ChromeDriver();
+    }
+
+    @After
+    public void teardown() {
+        this.driver.close();
+        this.driver.quit();
+    }
+
+    @Test
+    public void testLoginToHomePage() {
+        login(this.driver, testProperties.getUsername(), testProperties.getPassword());
+        WebElement homeElement = this.driver.findElement(By.tagName("home-view"));
+        homeElement.findElement(By.tagName("h3"));
+
+        Assert.assertEquals(WELCOME_TO_EMMY_APP, homeElement.getText());
+    }
+
+    protected void login(WebDriver driver, String username, String password) {
+        driver.navigate().to(ViewsStrings.LOGIN_URL);
+        WebElement loginElement = driver.findElement(By.id("vaadinLoginUsername"));
+        loginElement.findElement(By.xpath("input[@name = 'username']"));
+        Assert.assertNotNull(loginElement);
+        WebElement passwordElement = driver.findElement(By.id("vaadinLoginPassword"));
+        passwordElement.findElement(By.xpath("input[@name = 'password']"));
+        Assert.assertNotNull(passwordElement);
+
+        loginElement.sendKeys(username);
+        passwordElement.sendKeys(password);
+
+        WebElement loginBtn = driver.findElement(By.tagName("vaadin-button"));
+        loginBtn.click();
+        Assert.assertNotNull(loginBtn);
+
+        String currentUrl = this.driver.getCurrentUrl();
+        Assert.assertEquals(ViewsStrings.HOME_URL, currentUrl);
+    }
+}
---
+++ src/test/java/org/tpokora/views/ViewsStrings.java
@@ -0,0 +1,9 @@
+package org.tpokora.views;
+
+public class ViewsStrings {
+
+    public static final String BASIC_URL = "http://localhost:8080";
+
+    public static final String LOGIN_URL = BASIC_URL + "/login";
+    public static final String HOME_URL = BASIC_URL + "/home";
+}
