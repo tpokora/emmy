a2b955d4b52da41c47013e6bc619b0d73fbcf2e7
tpokora <pokora.tomek@gmail.com>
2019-10-22 22:01:50 +0200

    #26 Remove unnecessary @Repository annotation

--- src/main/java/org/tpokora/users/dao/UserRepository.java
+++ src/main/java/org/tpokora/users/dao/UserRepository.java
@@ -1,12 +1,10 @@
 package org.tpokora.users.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import org.tpokora.users.model.User;
 
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
 }
