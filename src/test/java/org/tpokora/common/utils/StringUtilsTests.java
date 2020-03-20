a1b8ce7b9bd1b1ebf91d5f7b8b6820826e8fca14
tpokora <pokora.tomek@gmail.com>
2020-03-20 19:59:24 +0100

    #26 String utils

---
+++ src/test/java/org/tpokora/common/utils/StringUtilsTests.java
@@ -0,0 +1,38 @@
+package org.tpokora.common.utils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StringUtilsTests {
+
+    @Test
+    public void testStringFirstLetterUppercase() {
+        String test = "test";
+        String expectedString = "Test";
+
+        Assertions.assertEquals(expectedString, StringUtils.makeFirstLetterUpperCase(test));
+    }
+
+    @Test
+    public void testStringFirstLetterUppercaseBlank() {
+        String test = "";
+        String expectedString = "";
+
+        Assertions.assertEquals(expectedString, StringUtils.makeFirstLetterUpperCase(test));
+    }
+
+    @Test
+    public void testStringFirstLetterUppercaseOneLetter() {
+        String test = "a";
+        String expectedString = "A";
+
+        Assertions.assertEquals(expectedString, StringUtils.makeFirstLetterUpperCase(test));
+    }
+
+    @Test
+    public void testStringFirstLetterUppercaseNull() {
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            StringUtils.makeFirstLetterUpperCase(null);
+        });
+    }
+}
