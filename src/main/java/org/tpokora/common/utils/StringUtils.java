ca0d5f857c6e6008450adcaf13cce74f9ae3dd64
tpokora <pokora.tomek@gmail.com>
2020-04-08 20:32:48 +0200

    Use UnaryOperator

--- src/main/java/org/tpokora/common/utils/StringUtils.java
+++ src/main/java/org/tpokora/common/utils/StringUtils.java
@@ -1,19 +1,19 @@
 package org.tpokora.common.utils;
 
 import java.util.Objects;
-import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 public class StringUtils {
 
     public static final String STRING_IS_NULL = "String is null!";
 
-    public static Function<String, String> makeFirstLetterUpperCase = StringUtils::makeFirstLetterUpperCase;
+    public static UnaryOperator<String> makeFirstLetterUpperCase = StringUtils::makeFirstLetterUpperCase;
 
     private static String makeFirstLetterUpperCase(String string) {
         Objects.requireNonNull(string, STRING_IS_NULL);
         if (string.length() <= 1) {
             return string.toUpperCase();
         }
-        return string.substring(0, 1).toUpperCase() + string.substring(1);
+        return string.substring(0, 1).toUpperCase().concat(string.substring(1));
     }
 }
