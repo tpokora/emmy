11941bdb5a68847f477df5d863ad2f2504ed21f4
tpokora <pokora.tomek@gmail.com>
2020-07-04 19:01:24 +0200

    #59 Remove NotificationProperties

--- src/main/java/org/tpokora/Application.java
+++ src/main/java/org/tpokora/Application.java
@@ -6,18 +6,17 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 import org.tpokora.config.properties.FirebaseProperties;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.tpokora.weather.properties.StormProperties;
-import org.tpokora.config.properties.NotificationProperties;
 
 @SpringBootApplication(scanBasePackages = { "org.tpokora" })
 @EnableScheduling
 @EnableJpaRepositories
 @EnableTransactionManagement
-@EnableConfigurationProperties({FirebaseProperties.class, NotificationProperties.class, StormProperties.class})
+@EnableConfigurationProperties({FirebaseProperties.class, StormProperties.class})
 public class Application extends SpringBootServletInitializer {
 
     @Bean
