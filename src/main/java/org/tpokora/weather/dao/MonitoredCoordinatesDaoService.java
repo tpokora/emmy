7ae4b72efc4fe3caac1b982eae784a5e0d4a3873
tpokora <pokora.tomek@gmail.com>
2020-07-05 17:42:55 +0200

    MonitoredCoordinatesDaoService

--- src/main/java/org/tpokora/weather/dao/MonitoredCoordinatesDaoService.java
+++ src/main/java/org/tpokora/weather/dao/MonitoredCoordinatesDaoService.java
@@ -3,7 +3,10 @@ package org.tpokora.weather.dao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.tpokora.users.model.User;
 import org.tpokora.weather.model.entity.MonitoredCoordinatesEntity;
+
+import java.util.List;
 
 @Service
 public class MonitoredCoordinatesDaoService {
@@ -19,5 +22,15 @@ public class MonitoredCoordinatesDaoService {
     public MonitoredCoordinatesEntity save(MonitoredCoordinatesEntity monitoredCoordinatesEntity) {
         LOGGER.info(">>> Saving {}", monitoredCoordinatesEntity.toString());
         return monitoredCoordinatesRepository.saveAndFlush(monitoredCoordinatesEntity);
+    }
+
+    public List<MonitoredCoordinatesEntity> findByUser(User user) {
+        LOGGER.info(">>> Looking for MonitoredCoordinatesEntity by user: {}", user.getUsername());
+        return monitoredCoordinatesRepository.findAllByUser_Id(user.getId());
+    }
+
+    public void deleteById(int id) {
+        LOGGER.info(">>> Delete MonitoredCoordinatesEntity with id: {}", id);
+        monitoredCoordinatesRepository.deleteById(id);
     }
 }
--- src/test/java/org/tpokora/weather/dao/MonitoredCoordinatesDaoServiceTest.java
+++ src/test/java/org/tpokora/weather/dao/MonitoredCoordinatesDaoServiceTest.java
@@ -9,6 +9,8 @@ import org.tpokora.users.dao.UserRepository;
 import org.tpokora.users.model.User;
 import org.tpokora.weather.model.entity.MonitoredCoordinatesEntity;
 
+import java.util.List;
+
 class MonitoredCoordinatesDaoServiceTest extends BaseServiceTest {
 
     @Autowired
@@ -43,5 +45,48 @@ class MonitoredCoordinatesDaoServiceTest extends BaseServiceTest {
         Assertions.assertEquals(monitoredCoordinatesEntity.getLatitude(), saved.getLatitude());
         Assertions.assertEquals(monitoredCoordinatesEntity.getLatitudeDM(), saved.getLatitudeDM());
         Assertions.assertEquals(monitoredCoordinatesEntity.getUser().getId(), saved.getUser().getId());
+    }
+
+    @Test
+    void testFindByUser() {
+        MonitoredCoordinatesEntity monitoredCoordinatesEntity = new MonitoredCoordinatesEntity();
+        monitoredCoordinatesEntity.setUser(USER);
+        monitoredCoordinatesEntity.setLongitude(11.1111);
+        monitoredCoordinatesEntity.setLongitudeDM(11.23);
+        monitoredCoordinatesEntity.setLatitude(22.2222);
+        monitoredCoordinatesEntity.setLatitudeDM(22.23);
+        monitoredCoordinatesDaoService.save(monitoredCoordinatesEntity);
+
+        User secondUser = new User();
+        userRepository.save(secondUser);
+
+        MonitoredCoordinatesEntity secondMonitoredCoordinatesEntity = new MonitoredCoordinatesEntity();
+        secondMonitoredCoordinatesEntity.setUser(secondUser);
+        secondMonitoredCoordinatesEntity.setLongitude(11.1111);
+        secondMonitoredCoordinatesEntity.setLongitudeDM(11.23);
+        secondMonitoredCoordinatesEntity.setLatitude(22.2222);
+        secondMonitoredCoordinatesEntity.setLatitudeDM(22.23);
+        monitoredCoordinatesDaoService.save(secondMonitoredCoordinatesEntity);
+
+        List<MonitoredCoordinatesEntity> monitoredCoordinatesDaoServiceByUser = monitoredCoordinatesDaoService.findByUser(USER);
+        Assertions.assertEquals(1, monitoredCoordinatesDaoServiceByUser.size());
+    }
+
+    @Test
+    void testDeleteById() {
+        MonitoredCoordinatesEntity monitoredCoordinatesEntity = new MonitoredCoordinatesEntity();
+        monitoredCoordinatesEntity.setUser(USER);
+        monitoredCoordinatesEntity.setLongitude(11.1111);
+        monitoredCoordinatesEntity.setLongitudeDM(11.23);
+        monitoredCoordinatesEntity.setLatitude(22.2222);
+        monitoredCoordinatesEntity.setLatitudeDM(22.23);
+        monitoredCoordinatesDaoService.save(monitoredCoordinatesEntity);
+
+        List<MonitoredCoordinatesEntity> monitoredCoordinatesDaoServiceByUser = monitoredCoordinatesDaoService.findByUser(USER);
+        Assertions.assertEquals(1, monitoredCoordinatesDaoServiceByUser.size());
+        monitoredCoordinatesDaoService.deleteById(monitoredCoordinatesEntity.getId());
+
+        monitoredCoordinatesDaoServiceByUser = monitoredCoordinatesDaoService.findByUser(USER);
+        Assertions.assertTrue(monitoredCoordinatesDaoServiceByUser.isEmpty());
     }
 }
