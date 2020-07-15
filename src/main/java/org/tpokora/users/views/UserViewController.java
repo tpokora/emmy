3268c2b4704d24f3119bf3e4e9c4459df304dfa3
tpokora <pokora.tomek@gmail.com>
2020-07-15 21:31:10 +0200

    #63 Navigation element template

--- src/main/java/org/tpokora/users/views/UserViewController.java
+++ src/main/java/org/tpokora/users/views/UserViewController.java
@@ -2,9 +2,12 @@ package org.tpokora.users.views;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.tpokora.users.model.UserDetailsImpl;
 import org.tpokora.users.services.UserDetailsServiceImpl;
 
 import static org.tpokora.users.views.UsersViewConstants.PROFILE_VIEW_TEMPLATE;
@@ -15,6 +18,8 @@ public class UserViewController {
 
     private final Logger LOGGER = LoggerFactory.getLogger(UsersViewController.class);
 
+    private UserDetailsImpl userDetails;
+
     private UserDetailsServiceImpl userDetailsService;
 
     public UserViewController(UserDetailsServiceImpl userDetailsService) {
@@ -24,6 +29,15 @@ public class UserViewController {
     @GetMapping(value = PROFILE_VIEW_URL, name = "profile")
     public String myProfile(Model model) {
         LOGGER.info(">>> Profile Page");
+        getUserDetails();
+        model.addAttribute("user", userDetails);
         return PROFILE_VIEW_TEMPLATE;
+    }
+
+    private void getUserDetails() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            userDetails = ((UserDetailsImpl)principal);
+        }
     }
 }
