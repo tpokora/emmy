d3eaa841afac0f3f9271e0df5e75a74b26fdea4d
tpokora <pokora.tomek@gmail.com>
2020-08-01 15:22:46 +0200

    #47 HomeControllerWebMvcTest

--- src/main/java/org/tpokora/home/views/HomeViewController.java
+++ src/main/java/org/tpokora/home/views/HomeViewController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import static org.tpokora.common.CommonConstants.SLASH;
 import static org.tpokora.home.views.HomeViewConstants.*;
@@ -15,7 +16,7 @@ public class HomeViewController {
     private final Logger LOGGER = LoggerFactory.getLogger(HomeViewController.class);
 
     @GetMapping(value = { SLASH, HOME_VIEW_URL }, name = HOME_VIEW)
-    public String home(Model model) {
+    public @ResponseBody String home(Model model) {
         LOGGER.info(">> HomeView");
         return HOME_VIEW;
     }
---
+++ src/test/java/org/tpokora/home/views/HomeControllerWebLayerTest.java
@@ -0,0 +1,31 @@
+package org.tpokora.home.views;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.tpokora.config.TestH2DatabaseConfiguration;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HomeViewController.class)
+@TestPropertySource(locations = { "classpath:application-test.properties", "classpath:application-db-test.properties"})
+@Import(TestH2DatabaseConfiguration.class)
+public class HomeControllerWebLayerTest {
+
+    public static final String HOME = "home";
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHome() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString(HOME)));
+    }
+}
