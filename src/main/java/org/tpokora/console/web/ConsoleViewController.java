e8683968e249b6df253c898e6585835fc18bc025
tpokora <pokora.tomek@gmail.com>
2020-08-04 12:34:54 +0200

    #47 Add PropertyForm Validation

--- src/main/java/org/tpokora/console/web/ConsoleViewController.java
+++ src/main/java/org/tpokora/console/web/ConsoleViewController.java
@@ -6,11 +6,14 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
 import org.tpokora.console.dao.AppPropertyService;
+import org.tpokora.console.web.forms.AddLocationForm;
+import org.tpokora.console.web.forms.AddPropertyForm;
 import org.tpokora.users.model.User;
 import org.tpokora.users.model.UserDetailsImpl;
 import org.tpokora.weather.dao.MonitoredCoordinatesDaoService;
@@ -20,9 +23,11 @@ import org.tpokora.weather.properties.OpenCageDataProperties;
 import org.tpokora.weather.services.location.ILocationService;
 import org.tpokora.weather.services.location.OpenCageDataLocationService;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 import static org.tpokora.console.web.ConsoleViewConstants.*;
+import static org.tpokora.home.views.HomeViewConstants.HOME_VIEW_URL;
 
 @Controller
 public class ConsoleViewController {
@@ -49,21 +54,40 @@ public class ConsoleViewController {
     @GetMapping(value = CONSOLE_VIEW_URL, name = CONSOLE_VIEW)
     public String home(Model model) {
         LOGGER.info(">> Console");
-        model.addAttribute("addLocationForm", new AddLocationForm());
-        model.addAttribute("monitoredCoords", monitoredCoordinatesDaoService.getAll());
-        model.addAttribute("appProperties", appPropertyService.getAllProperties());
+        initializeView(model);
         getUserDetails();
         return CONSOLE_VIEW_TEMPLATE;
     }
 
     @PostMapping(value = CONSOLE_VIEW_URL + "/addLocation")
-    public String addLocationMonitorByName(Model model, @ModelAttribute("locationForm") AddLocationForm locationForm) {
+    public String addLocationMonitorByName(Model model, AddLocationForm locationForm) {
         LOGGER.info(">> Add Location to monitor by name: {}", locationForm.getLocationName());
         getUserDetails();
         Optional<Location> optionalLocation = locationService.getLocationCoordinatesByName(locationForm.getLocationName());
         optionalLocation.ifPresent(this::saveLocationToMonitor);
         model.addAttribute("addLocationForm", new AddLocationForm());
         return "redirect:" + CONSOLE_VIEW_URL;
+    }
+
+    @PostMapping(value = CONSOLE_VIEW_URL + "/addProperty")
+    public String addProperty(Model model, @Valid @ModelAttribute("addPropertyForm") AddPropertyForm propertyForm,
+                              BindingResult bindingResult) {
+        getUserDetails();
+        if (bindingResult.hasErrors()) {
+            initializeView(model);
+            model.addAttribute("propertyFormError", "Fields Validation error");
+            return CONSOLE_VIEW_TEMPLATE;
+
+        }
+        model.addAttribute("addPropertyForm", new AddPropertyForm());
+        return "redirect:" + CONSOLE_VIEW_URL;
+    }
+
+    private void initializeView(Model model) {
+        model.addAttribute("addLocationForm", new AddLocationForm());
+        model.addAttribute("addPropertyForm", new AddPropertyForm());
+        model.addAttribute("monitoredCoords", monitoredCoordinatesDaoService.getAll());
+        model.addAttribute("appProperties", appPropertyService.getAllProperties());
     }
 
     private void saveLocationToMonitor(Location location) {
@@ -76,19 +100,8 @@ public class ConsoleViewController {
     private void getUserDetails() {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         if (principal instanceof UserDetails) {
-            userDetails = ((UserDetailsImpl)principal);
+            userDetails = ((UserDetailsImpl) principal);
         }
     }
 
-    public class AddLocationForm {
-        private String locationName;
-
-        public String getLocationName() {
-            return locationName;
-        }
-
-        public void setLocationName(String locationName) {
-            this.locationName = locationName;
-        }
-    }
 }
