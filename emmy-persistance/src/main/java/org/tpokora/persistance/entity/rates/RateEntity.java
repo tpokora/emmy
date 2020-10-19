246811fb890589a255ab817adc60579705e9c28c
tpokora <pokora.tomek@gmail.com>
2020-10-19 19:13:40 +0200

    #79 RateEntity model

--- emmy-persistance/src/main/java/org/tpokora/persistance/entity/rates/RateEntity.java
+++ emmy-persistance/src/main/java/org/tpokora/persistance/entity/rates/RateEntity.java
@@ -4,23 +4,23 @@ import org.tpokora.persistance.entity.common.AbstractEntity;
 
 import java.time.LocalDateTime;
 
-public class Rate extends AbstractEntity {
+public class RateEntity extends AbstractEntity {
 
     private String from;
     private String to;
     private Double value;
     private LocalDateTime timestamp;
 
-    public Rate() {}
+    public RateEntity() {}
 
-    public Rate(String from, String to, Double value, LocalDateTime timestamp) {
+    public RateEntity(String from, String to, Double value, LocalDateTime timestamp) {
         this.from = from;
         this.to = to;
         this.value = value;
         this.timestamp = timestamp;
     }
 
-    public Rate(Integer id, String from, String to, Double value, LocalDateTime timestamp) {
+    public RateEntity(Integer id, String from, String to, Double value, LocalDateTime timestamp) {
         super(id);
         this.from = from;
         this.to = to;
