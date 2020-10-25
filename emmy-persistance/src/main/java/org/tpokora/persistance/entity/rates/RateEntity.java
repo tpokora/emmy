3fa6b228b48ab0970d551dd1a82d09b12b46ccb9
tpokora <pokora.tomek@gmail.com>
2020-10-25 16:54:49 +0100

    #79 RateEntity

--- emmy-persistance/src/main/java/org/tpokora/persistance/entity/rates/RateEntity.java
+++ emmy-persistance/src/main/java/org/tpokora/persistance/entity/rates/RateEntity.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 
 public class RateEntity extends AbstractEntity {
 
+    private String name;
     private String from;
     private String to;
     private Double value;
@@ -13,18 +14,72 @@ public class RateEntity extends AbstractEntity {
 
     public RateEntity() {}
 
-    public RateEntity(String from, String to, Double value, LocalDateTime timestamp) {
+    public RateEntity(String name, String from, String to, Double value, LocalDateTime timestamp) {
+        this.name = name;
         this.from = from;
         this.to = to;
         this.value = value;
         this.timestamp = timestamp;
     }
 
-    public RateEntity(Integer id, String from, String to, Double value, LocalDateTime timestamp) {
+    public RateEntity(Integer id, String name, String from, String to, Double value, LocalDateTime timestamp) {
         super(id);
+        this.name = name;
         this.from = from;
         this.to = to;
         this.value = value;
         this.timestamp = timestamp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return "RateEntity{" +
+                "name='" + name + '\'' +
+                ", from='" + from + '\'' +
+                ", to='" + to + '\'' +
+                ", value=" + value +
+                ", timestamp=" + timestamp +
+                ", id=" + id +
+                '}';
     }
 }
