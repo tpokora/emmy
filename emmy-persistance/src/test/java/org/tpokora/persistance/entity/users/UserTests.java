0b100daa5d03ce35d74f4308da2de05bc015f4fe
tpokora <pokora.tomek@gmail.com>
2020-11-25 18:07:06 +0100

    UserTests

--- emmy-persistance/src/test/java/org/tpokora/persistance/entity/users/UserTests.java
+++ emmy-persistance/src/test/java/org/tpokora/persistance/entity/users/UserTests.java
@@ -2,34 +2,50 @@ package org.tpokora.persistance.entity.users;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.tpokora.persistance.entity.weather.MonitoredCoordinatesEntity;
+
+import java.util.Set;
 
 public class UserTests {
 
     @Test
     public void testUserToString() {
         int id = 1;
-        String testUser = "testUser";
+        String testUserName = "testUserName";
         String testEmail = "testEmail@email.com";
         String testPassword = "testPassword";
 
         String expectedString = String.format("User{id=%d, username='%s', password='%s', email='%s', roles=null}",
-                id, testUser, testPassword, testEmail);
+                id, testUserName, testPassword, testEmail);
 
-        User user = new User(testUser, testPassword, testEmail);
+        User user = new User(testUserName, testPassword, testEmail);
         user.setId(id);
 
         Assertions.assertEquals(expectedString, user.toString());
+
+        User testUser = new User(user);
+        expectedString = String.format("User{id=%d, username='%s', password='%s', email='%s', roles=null}",
+                testUser.getId(), testUser.getUsername(), testUser.getPassword(), testUser.getEmail());
+
+        Assertions.assertEquals(expectedString, testUser.toString());
+
+        String newTestUserName = "newTestUserName";
+        String newPassword = "newPassword";
+        String email = "newEmail@email.com";
+        testUser.setUsername(newTestUserName);
+        testUser.setPassword(newPassword);
+        testUser.setEmail(email);
+
+        expectedString = String.format("User{id=%d, username='%s', password='%s', email='%s', roles=null}",
+                testUser.getId(), testUser.getUsername(), testUser.getPassword(), testUser.getEmail());
+
+        Assertions.assertEquals(expectedString, testUser.toString());
+
+        testUser.setMonitoredCoordinateEntities(Set.of(new MonitoredCoordinatesEntity()));
+        testUser.setRoles(Set.of(new Role()));
+        
+        Assertions.assertFalse(testUser.getMonitoredCoordinateEntities().isEmpty());
+        Assertions.assertFalse(testUser.getRoles().isEmpty());
     }
 
-    @Test
-    public void testRoleToString() {
-        int id = 1;
-        String testName = "testRole";
-        Role role = new Role(testName);
-        role.setId(id);
-
-        String expectedString = String.format("Role{id=%d, name='%s'}", id, testName);
-
-        Assertions.assertEquals(expectedString, role.toString());
-    }
 }
