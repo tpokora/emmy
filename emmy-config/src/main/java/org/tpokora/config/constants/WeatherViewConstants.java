06e575ec9d2b02ab2a940833ff64fa84f29d9a7e
tpokora <pokora.tomek@gmail.com>
2020-12-31 11:45:56 +0100

    #81 WeatherDataViewController

--- emmy-config/src/main/java/org/tpokora/config/constants/WeatherViewConstants.java
+++ emmy-config/src/main/java/org/tpokora/config/constants/WeatherViewConstants.java
@@ -4,7 +4,9 @@ public class WeatherViewConstants {
 
     public static final String WEATHER_VIEW = "weather";
     public static final String WEATHER_VIEW_TEMPLATE = "weather/weather";
+    public static final String WEATHER_DATA_VIEW_TEMPLATE = "weather/weather_data";
     public static final String WEATHER_VIEW_URL = CommonConstants.SLASH + WEATHER_VIEW;
+    public static final String WEATHER_DATA_VIEW_URL = CommonConstants.SLASH + WEATHER_VIEW + "-data";
 
     public static final String WEATHER_FIND_LOCATION_URL = WEATHER_VIEW_URL + "/find-location";
     public static final String WEATHER_FIND_STORM_URL = WEATHER_VIEW_URL + "/find-storm";
