5279a91f798b7f5cf5f823785fe45f8caf534793
tpokora <pokora.tomek@gmail.com>
2021-01-06 16:01:33 +0100

    WeatherDataViewControllerWebTest

--- emmy-application/src/test/java/org/tpokora/application/weather/views/WeatherDataViewControllerWebTest.java
+++ emmy-application/src/test/java/org/tpokora/application/weather/views/WeatherDataViewControllerWebTest.java
@@ -3,7 +3,9 @@ package org.tpokora.application.weather.views;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.tpokora.application.common.views.BaseViewControllerWebTest;
+import org.tpokora.persistance.services.weather.ForecastDaoService;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,6 +14,9 @@ class WeatherDataViewControllerWebTest extends BaseViewControllerWebTest {
 
     @Autowired
     WeatherDataViewController weatherDataViewController;
+
+    @MockBean
+    private ForecastDaoService forecastDaoService;
 
     @Test
     void contextLoads() throws Exception {
