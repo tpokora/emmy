bbf5efa754a9ce829847f7b056039e71e6aaa94f
tpokora <pokora.tomek@gmail.com>
2021-01-06 09:30:31 +0100

    Test branch

--- emmy-application/src/test/java/org/tpokora/application/common/utils/FileReaderUtils.java
+++ emmy-application/src/test/java/org/tpokora/application/common/utils/FileReaderUtils.java
@@ -1,7 +1,13 @@
 package org.tpokora.application.common.utils;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -19,5 +25,25 @@ public class FileReaderUtils {
             e.printStackTrace();
         }
         return readString;
+    }
+
+    @Test
+    void loadJSONTest() {
+        String fileName = "weather/location/openCageDataResponse.json";
+
+        ClassLoader classLoader = getClass().getClassLoader();
+
+        try (InputStream inputStream = classLoader.getResourceAsStream(fileName);
+             InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+             BufferedReader reader = new BufferedReader(streamReader)) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
