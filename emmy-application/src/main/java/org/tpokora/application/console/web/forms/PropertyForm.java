a0a9784714e3a1e205846b28ac0d28639473856b
tpokora <pokora.tomek@gmail.com>
2021-01-14 22:24:35 +0100

    Edit property errors propagation

--- emmy-application/src/main/java/org/tpokora/application/console/web/forms/PropertyForm.java
+++ emmy-application/src/main/java/org/tpokora/application/console/web/forms/PropertyForm.java
@@ -7,17 +7,19 @@ import javax.validation.constraints.Size;
 
 public class PropertyForm extends BasicForm {
 
-    public static final String NAME = "Name";
+    public static final String NAME = "Name ";
+    public static final String VALUE = "Value ";
+    public static final String DESCRIPTION = "Description ";
 
     @NotNull(message = FIELD_REQUIRED)
     @Size(min=3, max=30, message = NAME + MIN_AND_MAX)
     private String name;
 
     @NotNull(message = FIELD_REQUIRED)
-    @Size(min=1, max=30, message = "Name" + MIN_AND_MAX)
+    @Size(min=1, max=30, message = VALUE + MIN_AND_MAX)
     private String value;
 
-    @Size(max=200, message = "Name" + MAX)
+    @Size(max=200, message = DESCRIPTION + MAX)
     private String description;
 
     public String getName() {
