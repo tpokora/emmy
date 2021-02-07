c34cb4268896b6107c2cf016832ab0da796845a8
tpokora <pokora.tomek@gmail.com>
2021-02-07 22:42:08 +0100

    #51 RatesHistoricViewController test

--- emmy-application/src/main/java/org/tpokora/application/rates/views/RatesHistoricViewController.java
+++ emmy-application/src/main/java/org/tpokora/application/rates/views/RatesHistoricViewController.java
@@ -2,11 +2,11 @@ package org.tpokora.application.rates.views;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.tpokora.application.rates.views.forms.RateForm;
 import org.tpokora.application.weather.views.DatesFormatterViewsHelper;
