656445983c7a62b3441b4f77a1c105841bbfb838
tpokora <pokora.tomek@gmail.com>
2021-02-07 18:19:25 +0100

    #51 Rates dataTable

--- emmy-application/src/main/java/org/tpokora/application/rates/views/RatesHistoricViewController.java
+++ emmy-application/src/main/java/org/tpokora/application/rates/views/RatesHistoricViewController.java
@@ -2,24 +2,37 @@ package org.tpokora.application.rates.views;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.tpokora.application.weather.views.DatesFormatterViewsHelper;
+import org.tpokora.persistance.entity.rates.RateEntity;
+import org.tpokora.persistance.services.rates.IRatesDaoService;
+
+import java.util.List;
 
 import static org.tpokora.application.rates.views.RatesViewConstants.*;
 
-@Controller(RATES)
+@Controller
 public class RatesHistoricViewController {
+
+    public static final String DATES_FORMATTER = "datesFormatter";
 
     private final Logger LOGGER = LoggerFactory.getLogger(RatesHistoricViewController.class);
 
-    public RatesHistoricViewController() {
+    private IRatesDaoService ratesDaoService;
 
+    public RatesHistoricViewController(@Qualifier("ratesDaoJpaService") IRatesDaoService ratesDaoService) {
+        this.ratesDaoService = ratesDaoService;
     }
 
     @GetMapping(value = HISTORIC_RATES, name = RATES_HISTORIC_VIEW_NAME)
     public String historicRatesHome(Model model) {
         LOGGER.info(">>> RatesViewController");
+        List<RateEntity> ratesDaysBeforeToday = ratesDaoService.getRatesDaysBeforeToday("XAU", "PLN", 30);
+        model.addAttribute(DATES_FORMATTER, new DatesFormatterViewsHelper());
+        model.addAttribute("rates", ratesDaysBeforeToday);
         return HISTORIC_RATES_TEMPLATE_URL;
     }
 }
--- emmy-application/src/main/java/org/tpokora/application/rates/views/RatesViewConstants.java
+++ emmy-application/src/main/java/org/tpokora/application/rates/views/RatesViewConstants.java
@@ -4,7 +4,7 @@ public class RatesViewConstants {
 
     public static final String RATES = "/rates";
 
-    public static final String HISTORIC_RATES = "/historicRates";
+    public static final String HISTORIC_RATES = RATES + "/historicRates";
 
     public static final String RATES_TEMPLATE_ROOT_URL = "rates";
     public static final String HISTORIC_RATES_TEMPLATE_URL = RATES_TEMPLATE_ROOT_URL + "/historic_rates";
