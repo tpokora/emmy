0e02d314eb7fd45383a9d6feb87e4205dab33cb9
tpokora <pokora.tomek@gmail.com>
2021-02-21 16:01:46 +0100

    Cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
-import HomeElement from './home/Welcome.js'
-import WelcomeElement from "./home/Welcome.js";
+import WelcomeElement from './home/Welcome.js'
 
 class App extends Component {
 
@@ -12,32 +11,31 @@ class App extends Component {
         fetch('http://localhost:8080/api/hello')
             .then(response => response.text())
             .then(message => {
-        this.setState({message: message});
-        });
+                this.setState({message: message});
+            });
     }
 
 
-
-render () {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="App-title">{this.state.message}</h1>
-        <p>
-          <WelcomeElement/>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">{this.state.message}</h1>
+                    <p>
+                        <WelcomeElement/>
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
