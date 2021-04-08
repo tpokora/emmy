3677cca0116e7e1983f0d26b6fa240a188311f5b
tpokora <pokora.tomek@gmail.com>
2021-04-08 19:32:22 +0200

    #89 Change WeatherElement to function

--- frontend/src/weather/Weather.js
+++ frontend/src/weather/Weather.js
@@ -1,82 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {properties} from '../properties'
-import {Box, Button, Container, Input} from "@material-ui/core";
+import {Box, Button, Container, Input, makeStyles} from "@material-ui/core";
 
-class WeatherElement extends React.Component {
 
-    state = {
-        value: '',
-        loading: true,
-        forecast: null,
-    };
+function WeatherElement() {
 
-    constructor() {
-        super();
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    const [locationName, setLocationName] = useState('')
+    const [loading, setLoading] = useState(true)
+    const [forecast, setForecast] = useState(null)
 
-    handleChange(event) {
-        this.setState({
-            value: event.target.value
-        });
-    }
+    const useStyles = makeStyles((theme) => ({
+        weatherForm: {
+            color: 'white'
+        }
+    }));
 
-    async handleSubmit(event) {
+    const classes = useStyles();
+
+    const submitForm = async function handleSubmit(event) {
         event.preventDefault();
-        let url = properties.emmyEndPoint + '/api/weather/forecast?location=' + this.state.value;
+        let url = properties.emmyEndPoint + '/api/weather/forecast?location=' + locationName;
         const response = await fetch(url);
         const data = await response.json();
-        this.setState({forecast: data, loading: false})
+        setForecast(data)
+        setLoading(false)
         console.log(data);
     }
 
-
-    render() {
-        return (
-            <Container maxWidth="false" disableGutters={true}>
-                <Box p={8}>
-                    <h1>Weather</h1>
-                    <div>
-                        <form onSubmit={this.handleSubmit}>
-                            <Input placeholder="Location" inputProps={{ 'aria-label': 'description' }} color="primary" />
-                            <Button color="primary" type="submit">Find</Button>
-                        </form>
-                    </div>
-                    <hr/>
-                    <div>
-                        {this.state.loading || !this.state.forecast ? (
-                            <div></div>
-                        ) : (
-                            <div id="forecastDetailsElement">
-                                <div className="row text-success">>> Forecast: <b
-                                    className="value">{this.state.forecast.name}</b>, <b
-                                    className="value">{this.state.forecast.description}</b></div>
-                                <div className="row text-success">>> Location: <b
-                                    className="value">{this.state.forecast.location}</b></div>
-                                <div className="row text-success">>> Temperature: <b
-                                    className="value">{this.state.forecast.temp}</b> C, feels
-                                    like: <b className="value">{this.state.forecast.feelTemp}</b> C
-                                </div>
-                                <div className="row text-success">>> Minimum temperature: <b
-                                    className="value">{this.state.forecast.minTemp}</b> C,
-                                    Maximum temperature: <b className="value">{this.state.forecast.maxTemp}</b> C
-                                </div>
-                                <div className="row text-success">>> Rain in 1h: <b
-                                    className="value">{this.state.forecast.rain1h}</b> mm, Rain
-                                    in 3h: <b className="value">{this.state.forecast.rain3h}</b> mm
-                                </div>
-                                <div className="row text-success">>> Pressure: <b
-                                    className="value">{this.state.forecast.pressure}</b> hPa,
-                                    Humidity: <b className="value">{this.state.forecast.humidity}</b>%, Wind: <b
-                                        className="value">{this.state.forecast.wind}</b> km/h
-                                </div>
+    return (
+        <Container maxWidth="false" disableGutters={true}>
+            <Box p={8}>
+                <h1>Weather</h1>
+                <div>
+                    <form onSubmit={submitForm}>
+                        <Input id="forecastFormInput" className={classes.weatherForm} placeholder="Location"
+                               value={locationName} onChange={e => setLocationName(e.target.value)}
+                               inputProps={{'aria-label': 'description'}} color="primary"/>
+                        <Button id="forecastFormBtn" color="primary" type="submit">Find</Button>
+                    </form>
+                </div>
+                <hr/>
+                <div>
+                    {loading || !forecast ? (
+                        <div></div>
+                    ) : (
+                        <div id="forecastDetailsElement">
+                            <div className="row text-success">>> Forecast: <b
+                                className="value">{forecast.name}</b>, <b
+                                className="value">{forecast.description}</b></div>
+                            <div className="row text-success">>> Location: <b
+                                className="value">{forecast.location}</b></div>
+                            <div className="row text-success">>> Temperature: <b
+                                className="value">{forecast.temp}</b> C, feels
+                                like: <b className="value">{forecast.feelTemp}</b> C
                             </div>
-                        )}
-                    </div>
-                </Box>
-            </Container>);
-    }
+                            <div className="row text-success">>> Minimum temperature: <b
+                                className="value">{forecast.minTemp}</b> C,
+                                Maximum temperature: <b className="value">{forecast.maxTemp}</b> C
+                            </div>
+                            <div className="row text-success">>> Rain in 1h: <b
+                                className="value">{forecast.rain1h}</b> mm, Rain
+                                in 3h: <b className="value">{forecast.rain3h}</b> mm
+                            </div>
+                            <div className="row text-success">>> Pressure: <b
+                                className="value">{forecast.pressure}</b> hPa,
+                                Humidity: <b className="value">{forecast.humidity}</b>%, Wind: <b
+                                    className="value">{forecast.wind}</b> km/h
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </Box>
+        </Container>);
 }
 
 export default WeatherElement;
