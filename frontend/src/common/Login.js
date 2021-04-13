c0901ffeae42c7dac84db5fe38eda96ea21f5206
tpokora <pokora.tomek@gmail.com>
2021-04-13 17:19:00 +0200

    #92 Simple Material Sign in form

--- frontend/src/common/Login.js
+++ frontend/src/common/Login.js
@@ -1,14 +1,93 @@
-import {Box, Container, Typography} from "@material-ui/core";
+import {
+    Box,
+    Button,
+    Checkbox,
+    Container,
+    FormControlLabel,
+    Grid,
+    makeStyles,
+    TextField,
+    Typography
+} from "@material-ui/core";
 import React from "react";
+import Link from "@material-ui/core/Link";
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
 
 function Login() {
+    const classes = useStyles();
 
     return (
-        <Container maxWidth="false" disableGutters={true}>
+        <Container maxWidth="xs" disableGutters={true}>
             <Box p={8}>
-                <Typography variant="h4" component="h1" gutterBottom>
-                    Login
-                </Typography>
+                <div className={classes.paper}>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <form className={classes.form} noValidate>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </div>
             </Box>
         </Container>)
 
--- frontend/src/common/Login.test.js
+++ frontend/src/common/Login.test.js
@@ -2,7 +2,7 @@ import {render, screen} from '@testing-library/react';
 import Login from "./Login";
 
 test('Login Form', () => {
-    render(<Login />)
-    const loginTitle = screen.getByText(/Login/i);
+    let wrapper = render(<Login />);
+    const loginTitle = screen.getByText(/Sign in/i);
     expect(loginTitle).toBeInTheDocument()
-})
+});
